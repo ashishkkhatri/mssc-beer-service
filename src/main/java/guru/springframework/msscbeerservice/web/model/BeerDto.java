a7bcc5afbfc2e728b8e89512436d6ab7936e235a
Ashish Khatri <ashishkkhatri@gmail.com>
2020-10-03 15:24:21 +0530

    Added jackson config for BeerDto.

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,9 +29,11 @@ public class BeerDto {
 	private Integer version;
 	
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
 	private OffsetDateTime createdDate;
 	
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
 	private OffsetDateTime lastModifiedDate;
 	
 	@NotBlank
@@ -42,8 +46,10 @@ public class BeerDto {
 	@NotNull
 	private Long upc;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
 	@Positive
 	@NotNull
 	private BigDecimal price;
+	
 	private Integer quantityOnHand;
 }
