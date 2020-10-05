004b2370d6455746e5a276933b09c9b05f1cd2c1
Ashish Khatri <ashishkkhatri@gmail.com>
2020-10-05 17:56:45 +0530

    added the changes to only use the embedded data.

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbeerservice.bootstrap;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,11 +9,14 @@ import org.springframework.stereotype.Component;
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner{
 	public static final String BEER_1_UPC="0766564332";
 	public static final String BEER_2_UPC="0766564336";
 	public static final String BEER_3_UPC="0766564334";
+	public static final UUID BEER_1_UUID = UUID.fromString("55f1ef01-07c7-4839-a811-579e4e0c6e2e");
+	public static final UUID BEER_2_UUID = UUID.fromString("584edfd5-a541-49b2-8e2f-0a6ce899e370");
+	public static final UUID BEER_3_UUID = UUID.fromString("52534feb-9a00-4a9a-b9f4-0d04a36a67a2");
 
 	private final BeerRepository beerRepository;
 	
